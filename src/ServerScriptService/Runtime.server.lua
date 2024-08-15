006948ade047df4776a94aad2d3fee74720bf109
alexitosex <alexchayt@gmail.com>
2024-08-15 01:02:16 -0700

    v2

--- src/ServerScriptService/Runtime.server.lua
+++ src/ServerScriptService/Runtime.server.lua
@@ -1,4 +1,6 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
+
 local Knit = require(ReplicatedStorage.Packages:WaitForChild("Knit"))
 
 Knit.AddServices(ServerStorage.Sources.Services)
